be249527168c32ea951fbc2c5f50eac2d72383d2
zch0423 <zch0423@foxmail.com>
2020-12-20 19:56:13 +0800

    report完成

--- JunitTest/TestCases/OuterTests.java
+++ JunitTest/TestCases/OuterTests.java
@@ -11,38 +11,24 @@ class OuterTests {
     void testOuter1() {
         System.out.println("testOuter1");
     }
-//    @Test
-//    void testOuter2() {
-//        System.out.println("testOuter2");
-//    }
     @Nested
     class InnerTests {
         @Test
         void testInner1() {
             System.out.println("testInner1");
         }
-
-//        @Test
-//        void testInner2() {
-//            System.out.println("testInner2");
-//        }
     }
-
+    
     static class LifecycleExtension implements TestInstancePostProcessor, TestInstancePreDestroyCallback {
         //这个lifecycleExtension作为一个测试类内部的类
-        //implements声明了接口
         @Override
         public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
-            // 应该是wish to post-process test instances
             System.out.println("postProcess: " + o);
         }
-
         @Override
         public void preDestroyTestInstance(ExtensionContext context) throws Exception{
             // preDestroy
             System.out.println("preDestroy: " + context.getRequiredTestInstance());
         }
-
-
     }
 }
